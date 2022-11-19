83e5296be3ef32e38507e82764d89021f01b777d
kistoo <tomio.nakamura15@gmail.com>
2022-11-18 19:31:24 -0500

    Add password validation

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const inputNodeList = document.querySelectorAll("input");
 const submitButton=document.querySelector("button");
 submitButton.addEventListener("click",()=>checkFields());
 
+let password = ""
+
 inputNodeList.forEach(input => {
     input.addEventListener("click", ()=>{
         input.classList.remove("error");
@@ -12,8 +14,31 @@ inputNodeList.forEach(input => {
 function checkFields() {
     inputNodeList.forEach(input => {
         let validity = input.checkValidity();
+        console.log(validity);
         if (validity===false) {
             input.classList.add("error");
+            input.setCustomValidity("Please complete this field");
+            if (input.id === "password" || input.id === "confirm-password") {
+                input.setCustomValidity("Password must be between 8 and 20 characters");
+            }
+        }
+        //validates that passwords match
+        if (input.id === "password") {
+            if (validity === true) {
+                password = input.value;
+            }
+        }
+        if (input.id === "confirm-password") {
+            if (input.value !== password) {
+                input.setCustomValidity("Passwords does not match");
+                input.classList.add("error");
+                console.log(password);
+            } else if (password.length < 7) {
+                input.setCustomValidity("Passwords must be between 8 and 20 characters");
+                input.classList.add("error");
+                console.log(password);
+
+            }
         }
     });
 }
